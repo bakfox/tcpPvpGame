9148159e27da0e4149fdbafbf09891e9a2f2fd0b
bakfox <153157605+bakfox@users.noreply.github.com>
2025-01-10 17:45:13 +0900

    feat : 첫로딩
    데이터 수정과 adduser deivceid 아이디로 실행

--- src/handlers/user/initialHandler.js
+++ src/handlers/user/initialHandler.js
@@ -3,6 +3,7 @@ import { crateUser, findUserByDeviceID, updateUserLogin } from "../../db/user/us
 import { addUser } from "../../session/userSession.js";
 import { handlerError } from "../../utils/error/errorHandler.js";
 import { createResponse } from "../../utils/response/createResponse.js";
+import { getAllGameSessions } from "../../session/gameSession.js";
 
 // 접속했을때 처음인지 아닌지를 판단해서 db에 저장하거나 새로 저장해주고 user클래스를 생성해줍니다!
 const initalHandler = async ({ socket, userID, payload }) => {
@@ -16,15 +17,23 @@ const initalHandler = async ({ socket, userID, payload }) => {
 			await updateUserLogin(user.id);
 		}
 
-		addUser(socket, user.id);
+		addUser(socket, deviceId);
 
+		const gameSessionsData = getAllGameSessions();
+
+		const gameSessions = gameSessionsData.map((games) => ({
+			id: games.id,
+			users: games.users.length,
+		}));
+		console.log(gameSessions);
 		const initialResponse = createResponse(
 			HANDLER_IDS.INITIAL,
 			RESPONSE_SUCCESS_CODE,
-			{ userId: user.id },
+			{ gameSessions },
 			deviceId
 		);
 
+		console.log(initialResponse);
 		socket.write(initialResponse);
 	} catch (error) {
 		handlerError(socket, error);
